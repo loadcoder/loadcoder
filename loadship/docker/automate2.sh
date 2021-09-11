13ba90771e06a8cf06073aa6138366c7077b36c8
StefanVahlgren <stefan.vahlgren@gmail.com>
2021-09-11 12:16:26 +0200

    last adjustments for the postbuil cluster test

--- loadship/docker/automate2.sh
+++ loadship/docker/automate2.sh
@@ -4,7 +4,9 @@ sudo docker container rm loadship
 
 sudo docker image rm loadship:1.0.0
 
-cp ../target/loadship-1.0.0-SNAPSHOT.jar loadship.jar
+#cp ../target/loadship-1.0.0-SNAPSHOT.jar loadship.jar
 
-sudo docker build -t loadcoder:1.0.0 .
+cd ..
+
+sudo docker build -f docker/Dockerfile -t loadcoder:1.0.0 .
 
